1405723ade1ba2e43752ae169f59d9160ea021be
Satyam Ghodasara <sghodas@gmail.com>
2017-09-12 09:14:53 -0400

    Added matchImages.m

---
+++ matchImages.m
@@ -0,0 +1,18 @@
+function [ corrCoefs ] = matchImages( images, dict )
+%matchImages Finds the best perfusion curve in the dictionary for each
+%voxel in images.
+%   
+
+if size(images, 4) ~= size(dict, 1)
+    error(['Error. Dimension mismatch between images (%d columns) and ' ...
+        'dict (%d rows)'], size(images, 4), size(dict, 1));
+end
+
+unrolledImages = unrollImages(images, size(images, 4));
+
+% Normalized dot product with mean centering
+corrCoefs = normr(unrolledImages - mean(unrolledImages)) * normc(dict - mean(dict));
+
+
+end
+
