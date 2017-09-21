2d6f70e18f19f46d3f7e3ceb40ef47e2ee83aadd
Satyam Ghodasara <sghodas@gmail.com>
2017-09-21 14:20:40 -0400

    Added function to retrieve perfusion parameters

---
+++ getPerfusionParameters.m
@@ -0,0 +1,15 @@
+function [ maxCorrCoef, af, dv, mtt ] = getPerfusionParameters( corrCoefs, afRange, dvRange, mttRange )
+%getPerfusionParameters Gets the perfusion parameter values.
+%   Gets the perfusion parameter values from the dictionary characteristics.
+
+% Get the max correlation coefficient and its associated index
+[maxCorrCoef, i] = max(corrCoefs);
+
+% Get the perfusion parameters
+[iAF, iDV, iMTT] = ind2sub([length(afRange), length(dvRange), length(mttRange)], i);
+af = afRange(iAF);
+dv = dvRange(iDV);
+mtt = mttRange(iMTT);
+
+end
+
