31a9ac38f3c7280a29c28244e4f1178c9c0733ec
Satyam Ghodasara <sghodas@gmail.com>
2017-10-02 14:55:19 -0400

    Added section headers for comments

--- generateDictionary.m
+++ generateDictionary.m
@@ -4,7 +4,7 @@ function [ D ] = generateDictionary( times, AF, PV, alpha, TR, baseFrame, frames
 %   curves made by all combinations of perfusion parameters. Inputs are
 %   specific to each data set.
 
-% Constants
+%% Constants
 %   T10 values are in milliseconds
 alpha = alpha * pi / 180;
 T10b = 1.664 * 1000;
@@ -18,7 +18,7 @@ relaxivity = 6.3;
 
 %startpoint = 2;% can get rid of some points at the beginning where there is no contrast
 
-% Ca calculation
+%% Ca calculation
 % Yong's code
 S0b = mean(PV(baseFrame:baseFrame+frames)) * (1 - exp(-R10b * TR) * cos(alpha)) / (1 - exp(-R10b * TR)) / sin(alpha);
 R1b = log((S0b * sin(alpha) - AF .* cos(alpha)) ./ (S0b * sin(alpha) - AF)) / TR;
@@ -26,27 +26,27 @@ Cb_artery = (R1b - R10b) * 1e3 / relaxivity; % Concentration in blood (mM)
 Cb_plasma = Cb_artery / (1 - Hct); % Concentration in plasma of artery (mM)
 Cb_plasma(1:baseFrame) = 0;
 
-% Cpv calculation
+%% Cpv calculation
 S0p = mean(PV(baseFrame:baseFrame+frames)) * (1 - exp(-R10p * TR) * cos(alpha)) / (1 - exp(-R10p * TR)) / sin(alpha); %GE equation
 R1p = log((S0p * sin(alpha) - PV .* cos(alpha)) ./ (S0p * sin(alpha) - PV)) / TR;
 Cp_artery = (R1p - R10p) * 1e3 / relaxivity; % Concentration in portal vein (mM)
 Cp_plasma = Cp_artery / (1 - Hct); % Concentration in plasma of portal vein (mM)
 Cp_plasma(1:baseFrame) = 0;
 
-% CL calculation
+%% CL calculation
 % S0L = mean(Liver(baseFrame:baseFrame+frames)) * (1 - exp(-R10L * TR) * cos(alpha)) / (1 - exp(-R10L * TR)) / sin(alpha); %GE equation
 % R1L = abs(log((S0L * sin(alpha) - Liver .* cos(alpha)) ./ (S0L * sin(alpha) - Liver)) / TR);
 % CL = (R1L - R10L) * 1e3 / relaxivity; % Concentration in liver
 % % CL = CL * 0.2627; %0.2627 = 0.22/mean(CL(end-5:end)); normalize the liver SI
 
-% Tau calculation
+%% Tau calculation
 delayFrames = abs(find(Cb_plasma ~= 0, 1) - find(CL ~= 0, 1));
 timePerFrame = times(2) - times(1);
 delayTime = delayFrames * timePerFrame;
 tauA = delayTime;
 tauP = delayTime;
 
-% Ranges for perfusion parameters
+%% Ranges for perfusion parameters
 AF_range = linspace(0, 1, 101);
 DV_range = linspace(0, 1, 21);
 MTT_range = linspace(1, 100, 100);
