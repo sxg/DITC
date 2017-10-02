08b4542bf17f3a48550cb4809a426a6c3a80a151
Satyam Ghodasara <sghodas@gmail.com>
2017-10-02 14:36:34 -0400

    getPerfusionParameters now returns perfusion parameters for all max values instead of just the first max value

--- getPerfusionParameters.m
+++ getPerfusionParameters.m
@@ -1,12 +1,18 @@
-function [ maxCorrCoef, af, dv, mtt ] = getPerfusionParameters( corrCoefs, afRange, dvRange, mttRange )
+function [ maxCorrCoef, af, dv, mtt ] = getPerfusionParameters( ... 
+    corrCoefs, afRange, dvRange, mttRange )
 %getPerfusionParameters Gets the perfusion parameter values.
 %   Gets the perfusion parameter values from the dictionary characteristics.
 
-% Get the max correlation coefficient and its associated index
-[maxCorrCoef, i] = max(corrCoefs);
+% Get the max correlation coefficient and its associated index(es)
+% Note: correlation coefficients are read to 4 decimal places to determine
+% if they're identical.
+maxCorrCoef = max(corrCoefs);
+indexes = find(corrCoefs == round(maxCorrCoef, 4));
+
 
 % Get the perfusion parameters
-[iAF, iDV, iMTT] = ind2sub([length(afRange), length(dvRange), length(mttRange)], i);
+[iAF, iDV, iMTT] = ind2sub([length(afRange), length(dvRange), ...
+    length(mttRange)], indexes);
 af = afRange(iAF);
 dv = dvRange(iDV);
 mtt = mttRange(iMTT);
