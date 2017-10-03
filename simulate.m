5b8f3c3bb5a20dddd1081a0e51925273f7a0c6d5
Satyam Ghodasara <sghodas@gmail.com>
2017-10-03 14:31:46 -0400

    Added a timer to the Monte Carlo simulation

--- simulate.m
+++ simulate.m
@@ -1,5 +1,5 @@
 function [noisyCurves, fitCurves, fitCurveIndexes, fitPerfParams, ...
-          fitCorrCoefs] = simulate(dict, curve, afRange, dvRange, ...
+          fitCorrCoefs, time] = simulate(dict, curve, afRange, dvRange, ...
                                    mttRange, nSims, snr)
 %simulate Runs a Monte Carlo simulation of the dictionary fitting.
 
@@ -17,6 +17,7 @@ fitCurves = NaN(nSims, size(dict, 1));
 fitCurveIndexes = NaN(nSims, 1);
 fitPerfParams = NaN(nSims, 6); % af, dv, mtt, k1a, k1p, k2 (in order)
 fitCorrCoefs = NaN(nSims, 1);
+time = NaN(nSims, 1);
 
 %% Run the Monte Carlo simulations
 
@@ -24,11 +25,15 @@ dispstat('', 'init');
 for sim = 1:nSims
     dispstat(sprintf('%d %%', round(sim / nSims * 100)));
     
-    % Add noise and match the curve
+    % Add noise
     noisyCurve = addNoise(curve, snr);
+    tic; % Start the timer
+    % Match the curve
     corrCoefs = matchCurve(noisyCurve, dict);
-    [af, dv, mtt, k1a, k1p, k2, index, maxCorrCoef] = getPerfusionParameters( ...
-        corrCoefs, afRange, dvRange, mttRange);
+    [af, dv, mtt, k1a, k1p, k2, index, maxCorrCoef] = ... 
+        getPerfusionParameters(corrCoefs, afRange, dvRange, mttRange);
+    t = toc; % Stop the timer
+    
     
     % Store the data
     noisyCurves(sim, :) = noisyCurve';
@@ -36,13 +41,14 @@ for sim = 1:nSims
     fitCurveIndexes(sim) = index;
     fitPerfParams(sim, :) = [af, dv, mtt, k1a, k1p, k2];
     fitCorrCoefs(sim) = maxCorrCoef;
+    time(sim) = t;
 end
 
 %% Save the data
 
 fileName = sprintf('SNRLevel-%d.mat', snr);
 save(fileName, 'noisyCurves', 'fitCurves', 'fitCurveIndexes', ...
-    'fitPerfParams', 'fitCorrCoefs');
+    'fitPerfParams', 'fitCorrCoefs', 'time');
 
 end
 
