bcbad0d0b2bb60ec1f254d6dd1fe3caad2bbccce
Satyam Ghodasara <sghodas@gmail.com>
2017-10-02 21:33:08 -0400

    Input handling can now auto transpose the curve

--- matchCurve.m
+++ matchCurve.m
@@ -2,8 +2,11 @@ function [ corrCoefs ] = matchCurve( curve, dict )
 %matchCurve Finds the best curve in a dictionary fitting a given curve.
 
 % Input validation
-validateattributes(curve, {'numeric'}, {'row'});
+validateattributes(curve, {'numeric'}, {'vector'});
 validateattributes(dict, {'numeric'}, {'2d', 'nonempty', 'nonsparse'});
+if ~isrow(curve)
+    curve = curve';
+end
 
 % Normalized dot product with mean centering
 corrCoefs = normr(curve - mean(curve)) * normc(dict - mean(dict));
