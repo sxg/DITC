ba45523e42ec56d0ad4edf5eaf8f09ffa84b18fe
Satyam Ghodasara <sghodas@gmail.com>
2017-10-03 11:33:18 -0400

    Added simulate function to run Monte Carlo simulations

---
+++ simulate.m
@@ -0,0 +1,52 @@
+function [noisyCurves, fitCurves, fitCurveIndexes, fitPerfParams, ...
+    fitCorrCoefs] = simulate(dict, curve, nSims, snr)
+%simulate Runs a Monte Carlo simulation of the dictionary fitting.
+
+%% Setup
+
+% Input validation
+validateattributes(dict, {'numeric'}, {'2d', 'nonempty', 'nonsparse'});
+validateattributes(curve, {'numeric'}, {'vector'});
+validateattributes(nSims, {'numeric'}, {'scalar'});
+validateattributes(snr, {'numeric'}, {'scalar'});
+
+% Create outputs
+noisyCurves = NaN(nSims, size(dict, 1));
+fitCurves = NaN(nSims, size(dict, 1));
+fitCurveIndexes = NaN(nSims, 1);
+fitPerfParams = NaN(nSims, 6); % af, dv, mtt, k1a, k1p, k2 (in order)
+fitCorrCoefs = NaN(nSims, 1);
+
+% Setup ranges
+afRange = linspace(0, 1, 101);
+dvRange = [0.1886];
+mttRange = linspace(1, 100, 100);
+
+%% Run the Monte Carlo simulations
+
+dispstat('', 'init');
+for sim = 1:nSims
+    dispstat(sprintf('%d \%', round(sim / nSims * 100)));
+    
+    % Add noise and match the curve
+    noisyCurve = addNoise(curve, snr);
+    corrCoefs = matchCurve(noisyCurve, dict);
+    [af, dv, mtt, index, maxCorrCoef] = getPerfusionParameters( ...
+        corrCoefs, afRange, dvRange, mttRange);
+    
+    % Store the data
+    noisyCurves(sim, :) = noisyCurves';
+    fitCurves(sim, :) = dict(:, index)';
+    fitCurveIndexes(i) = index;
+    fitPerfParams(i, 1:3) = [af, dv, mtt];
+    fitCorrCoefs(i) = maxCorrCoef;
+end
+
+%% Save the data
+
+fileName = sprintf('SNRLevel-%d.mat', snr);
+save(fileName, noisyCurves, fitCurves, fitCurveIndexes, fitPerfParams, ...
+    fitCorrCoefs);
+
+end
+
