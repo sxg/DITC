755f15ec352cff3ee4916c1b3a8439bcc392f53d
Satyam Ghodasara <sghodas@gmail.com>
2017-10-13 08:34:10 -0400

    Updated input validation for matchCurve

--- matchCurve.m
+++ matchCurve.m
@@ -2,15 +2,12 @@ function [ corrCoefs ] = matchCurve( curve, dict )
 %matchCurve Finds the best curve in a dictionary fitting a given curve.
 
 % Input validation
-validateattributes(curve, {'numeric'}, {'vector'});
+validateattributes(curve, {'numeric'}, {'column'});
 validateattributes(dict, {'numeric'}, {'2d', 'nonempty', 'nonsparse'});
-if ~isrow(curve)
-    curve = curve';
-end
 
 % Normalized dot product with mean centering
 % corrCoefs = normr(curve - mean(curve)) * normc(dict - mean(dict));
-corrCoefs = curve * dict;
+corrCoefs = curve' * dict;
 
 end
 
