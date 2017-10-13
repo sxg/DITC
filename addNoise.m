5d93cf60666c462998859fec97b6310c1a455de9
Satyam Ghodasara <sghodas@gmail.com>
2017-10-13 08:52:48 -0400

    addNoise now generates any number of noisy curves

--- addNoise.m
+++ addNoise.m
@@ -1,9 +1,20 @@
-function [y] = addNoise(x, snr)
+function [y] = addNoise(x, numCurves, snr)
 %addNoise Adds random noise to a vector.
 
-y = awgn(x, snr);
-% Alternative approach specified by Katie
-% y = x + randn(size(x)) * snr;
+% Input validation
+validateattributes(x, {'numeric'}, {'column', 'nonempty'});
+validateattributes(numCurves, {'numeric'}, {'scalar'});
+validateattributes(snr, {'numeric'}, {'scalar'});
+
+% Create the output
+y = NaN(size(x, 1), numCurves);
+
+% Generate noisy curves
+for i = 1:numCurves
+    y(:, i) = awgn(x, snr);
+    % Alternative approach specified by Katie
+    % y = x + randn(size(x)) * snr;
+end
 
 end
 
