fd3f6cedd103be62f57b965ae2d69d1699a5509e
Satyam Ghodasara <sghodas@gmail.com>
2017-10-13 09:04:39 -0400

    addNoise now saves the noisy curves

--- addNoise.m
+++ addNoise.m
@@ -1,20 +1,23 @@
-function [y] = addNoise(x, numCurves, snr)
+function [noisyCurves] = addNoise(curve, numCurves, snr)
 %addNoise Adds random noise to a vector.
 
 % Input validation
-validateattributes(x, {'numeric'}, {'column', 'nonempty'});
+validateattributes(curve, {'numeric'}, {'column', 'nonempty'});
 validateattributes(numCurves, {'numeric'}, {'scalar'});
 validateattributes(snr, {'numeric'}, {'scalar'});
 
 % Create the output
-y = NaN(size(x, 1), numCurves);
+noisyCurves = NaN(size(curve, 1), numCurves);
 
 % Generate noisy curves
 for i = 1:numCurves
-    y(:, i) = awgn(x, snr);
+    noisyCurves(:, i) = awgn(curve, snr);
     % Alternative approach specified by Katie
-    % y = x + randn(size(x)) * snr;
+    % noisyCurves = curve + randn(size(curve)) * snr;
 end
+
+% Save the noisy curves
+save(sprintf('NoisyCurves-SNR-%d.mat', snr), 'noisyCurves');
 
 end
 
