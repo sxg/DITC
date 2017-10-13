fe9aab297614877257acd468f61278c88b0ce153
Satyam Ghodasara <sghodas@gmail.com>
2017-10-13 08:29:48 -0400

    Added input validation to fitCurve

--- fitCurve.m
+++ fitCurve.m
@@ -2,6 +2,16 @@ function [af, dv, mtt, k1a, k1p, k2, err] = fitCurve(curve, times, ca, ...
     cp, tauA, tauP)
 %fitCurve Calculates the best fit curve.
 
+% Input validation
+validateattributes(curve, {'numeric'}, {'column'});
+validateattributes(times, {'numeric'}, ...
+    {'column', 'nonempty', 'increasing'});
+validateattributes(ca, {'numeric'}, {'column', 'nonempty'});
+validateattributes(cp, {'numeric'}, {'column', 'nonempty'});
+validateattributes(tauA, {'numeric'}, {'scalar'});
+validateattributes(tauP, {'numeric'}, {'scalar'});
+
+
 opts = optimset('Tolx', 1e-16, 'Tolfun', 1e-10, 'Display', 'off', ...
                 'DiffMinChange', 0.001);
 x0 = [0.2, 0.2, 10]; % af, dv, mtt
