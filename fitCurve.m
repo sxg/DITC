38fa2e1e5d3e2bbf8543f8296b7e9f4b31cc3425
Satyam Ghodasara <sghodas@gmail.com>
2017-10-18 11:03:01 -0400

    Added parallelization to lsqcurvefit options

--- fitCurve.m
+++ fitCurve.m
@@ -13,7 +13,7 @@ validateattributes(tauP, {'numeric'}, {'scalar'});
 
 
 opts = optimset('Tolx', 1e-16, 'Tolfun', 1e-10, 'Display', 'off', ...
-                'DiffMinChange', 0.001);
+                'DiffMinChange', 0.001, 'UseParallel', true);
 x0 = [0.2, 0.2, 10]; % af, dv, mtt
 nData = size(times, 1);
 xdata = [times, ca, cp, repmat(tauA, nData, 1), repmat(tauP, nData, 1)];
@@ -22,8 +22,7 @@ lb = [0, 0, 1];
 ub = [1, 1, 100];
 % [af, dv, mtt, err] = lsqcurvefit(@discLSWrapper, x0, xdata, ydata, lb, ...
 %     ub, opts);
-[x, err] = lsqcurvefit(@discLSWrapper, x0, xdata, ydata, lb, ...
-    ub, opts);
+[x, err] = lsqcurvefit(@discLSWrapper, x0, xdata, ydata, lb, ub, opts);
 af = x(1);
 dv = x(2);
 mtt = x(3);
