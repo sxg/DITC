a22e4ba8612a4bb7542e16f8bad5e31d71526b7b
Satyam Ghodasara <sghodas@gmail.com>
2017-10-19 11:10:13 -0400

    Added signal to concentration function for the liver

---
+++ concLiver.m
@@ -0,0 +1,17 @@
+function [cL] = concLiver(liverInputFunc, liverStart)
+%concLiver Contrast concentration in the liver tissue.
+
+alpha = 15 * pi / 180;
+TR = 5.12;
+T10l = 747;
+R10l = 1/T10l;
+relaxivity = 6.3;
+
+S0l = mean(liverInputFunc(1:liverStart)) ...
+    * (1 - exp(-R10l * TR) * cos(alpha)) / (1 - exp(-R10l * TR)) ...
+    / sin(alpha);
+R1l = abs(log((S0l * sin(alpha) - liverInputFunc .* cos(alpha)) ...
+    ./ (S0l * sin(alpha) - liverInputFunc)) / TR);
+cL = (R1l - R10l) * 1e3 / relaxivity;
+
+end
