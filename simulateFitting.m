c0a24b984d2f8cc3686e7ccc69821cf5becfb588
Satyam Ghodasara <sghodas@gmail.com>
2017-10-23 10:07:29 -0400

    Updated variable names and added tauA and tauP to saved perfusion parameters

--- simulateFitting.m
+++ simulateFitting.m
@@ -1,5 +1,5 @@
 function [fitCurves, fitErrs, fitPerfParams, fitTime] ...
-    = simulateFitting(noisyCurves, times, AF, PV, snr)
+    = simulateFitting(noisyCurves, times, artInputFunc, pvInputFunc, snr)
 %simulateFitting Runs Monte Carlo simulations of least squares fitting.
 
 %% Setup
@@ -9,26 +9,26 @@ validateattributes(noisyCurves, {'numeric'}, ...
     {'2d', 'nonempty', 'nonsparse'});
 validateattributes(times, {'numeric'}, ...
     {'column', 'nonempty', 'increasing'});
-validateattributes(AF, {'numeric'}, {'column', 'nonempty'});
-validateattributes(PV, {'numeric'}, {'column', 'nonempty'});
+validateattributes(artInputFunc, {'numeric'}, {'column', 'nonempty'});
+validateattributes(pvInputFunc, {'numeric'}, {'column', 'nonempty'});
 validateattributes(snr, {'numeric'}, {'scalar'});
 
 % Create outputs
 nSims = size(noisyCurves, 2);
 fitCurves = NaN(size(noisyCurves));
-fitErrs = NaN(nSims, 1);
-fitPerfParams = NaN(6, nSims); % af, dv, mtt, k1a, k1p, k2 (in order)
-fitTime = NaN(nSims, 1);
+fitErrs = NaN(1, nSims);
+fitPerfParams = NaN(8, nSims); % af, dv, mtt, k1a, k1p, k2 (in order)
+fitTime = NaN(1, nSims);
 
 % Calculate the contrast concentrations
 baseFrame = 1;
-[~, startFrame] = firstSignificant(PV);
-ca = cbPlasma(AF, PV, baseFrame, startFrame);
-cp = cpPlasma(PV, baseFrame, startFrame); 
+[~, startFrame] = firstSignificant(pvInputFunc);
+concAorta = cbPlasma(artInputFunc, pvInputFunc, baseFrame, startFrame);
+concPV = cpPlasma(pvInputFunc, baseFrame, startFrame); 
 
 % Calculate tau (look at Chouhan's paper for a better implementation)
-[~, i1] = firstSignificant(ca);
-[~, i2] = firstSignificant(cp);
+[~, i1] = firstSignificant(concAorta);
+[~, i2] = firstSignificant(concPV);
 dt = abs(times(2) - times(1));
 delayFrames = abs(i1 - i2);
 delayTime = delayFrames * dt;
@@ -46,14 +46,14 @@ for sim = 1:nSims
     tic; % Start the timer
     % Fit the curve
     [af, dv, mtt, k1a, k1p, k2, err] = fitCurve(noisyCurve, times, ...
-        ca, cp, tauA, tauP);
+        concAorta, concPV, tauA, tauP);
     t = toc; % Stop the timer
     
     % Store the data
     fitCurves(:, sim) = ...
-        normc(disc(times, ca, cp, af, dv, mtt, tauA, tauP));
+        normc(disc(times, concAorta, concPV, af, dv, mtt, tauA, tauP));
     fitErrs(sim) = err;
-    fitPerfParams(:, sim) = [af, dv, mtt, k1a, k1p, k2]';
+    fitPerfParams(:, sim) = [af, dv, mtt, tauA, tauP, k1a, k1p, k2]';
     fitTime(sim) = t;
 end
 
