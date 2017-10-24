f5e7a4d6cd56681ddd794416e054e9b8090f31c5
Satyam Ghodasara <sghodas@gmail.com>
2017-10-24 11:26:53 -0400

    RMSE now requires vectors

--- rmse.m
+++ rmse.m
@@ -2,9 +2,9 @@ function [err] = rmse(x, y)
 %rmse Calculates the root mean square error for two vectors.
 
 % Input validation
-validateattributes(x, {'numeric'}, {'nonempty'});
-validateattributes(y, {'numeric'}, {'nonempty'});
+validateattributes(x, {'numeric'}, {'vector', 'nonempty'});
+validateattributes(y, {'numeric'}, {'vector', 'nonempty'});
 
-err = sqrt(mean((x(:) - y(:)).^2));
+err = sqrt(mean((x - y).^2));
 
 end
