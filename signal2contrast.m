f8133e5b280966553b0785a994a1cbedcbaa89d1
Satyam Ghodasara <sghodas@gmail.com>
2017-10-30 22:54:46 -0400

    Reverted updates to signal2contrast

--- signal2contrast.m
+++ signal2contrast.m
@@ -4,34 +4,16 @@ function [contrast] = signal2contrast(signal)
 alpha = 15 * pi / 180;
 TR = 5.12;
 T10l = 800;
+R10l = 1/T10l;
 relaxivity = 6.3;
-Mo = 0.0029;
 
-% Signal --> concentration
-% e1l = exp(-TR / T10l);
-% M0l = mean(liverInputFunc(2:2+liverStart)) * (1 - cos(alpha) * e1l) ./ (sin(alpha) * (1 - e1l));
-% E1l = (liverInputFunc - M0l * sin(alpha)) ./ (liverInputFunc .* cos(alpha) - M0l .* sin(alpha));
-% T1l = -TR ./ log(E1l);
-% concLiver = (1 ./ (relaxivity / 1000)) .* ((1 ./ T1l) - (1 ./ T10l));
+liverStart = 10;
 
-% Signal --> contrast
-E1 = (signal - Mo * sin(alpha)) ./ ...
-    (signal .* cos(alpha) - Mo .* sin(alpha));
-T1l = -TR ./ log(E1);
-contrast = (1 / (relaxivity / 1000)) .* ((1 ./ T1l) - ( 1/ T10l));
-
-% Original code
-% S0l = mean(liverInputFunc(2:2+liverStart)) ...
-%     * (1 - exp(-R10l * TR) * cos(alpha)) / (1 - exp(-R10l * TR)) ...
-%     / sin(alpha);
-% R1l = abs(log((S0l * sin(alpha) - liverInputFunc .* cos(alpha)) ...
-%     ./ (S0l * sin(alpha) - liverInputFunc)) / TR);
-% concLiver = (R1l - R10l) * 1e3 / relaxivity;
-
-% Concentration --> signal
-% M0l = mean(liverInputFunc(2:2+liverStart));
-% T1l = 1 ./ (1 ./ T10l + relaxivity * liverInputFunc / 1000);
-% e1l = exp(-TR ./ T1l);
-% signal = M0l .* sin(alpha) .* (1 - e1l) ./ (1 - cos(alpha) .* e1l);
+S0l = mean(signal(2:2+liverStart)) ...
+    * (1 - exp(-R10l * TR) * cos(alpha)) / (1 - exp(-R10l * TR)) ...
+    / sin(alpha);
+R1l = abs(log((S0l * sin(alpha) - signal .* cos(alpha)) ...
+    ./ (S0l * sin(alpha) - signal)) / TR);
+contrast = (R1l - R10l) * 1e3 / relaxivity;
 
 end
