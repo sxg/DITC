ceed7854001fd48de55f37e55a848f08120e829b
Satyam Ghodasara <sghodas@gmail.com>
2017-10-31 22:49:47 -0400

    Switched to a 2-parameter dictionary for Monte Carlo simulations

--- matchCurve.m
+++ matchCurve.m
@@ -20,17 +20,15 @@ corrCoefs = normc(curve - mean(curve))' * nmcDict;
 %% Get the perfusion parameters
 % Get the max correlation coefficient and its associated index
 [maxCorrCoef, index] = max(corrCoefs);
-[iAF, iDV, iMTT] = ind2sub([length(afRange), length(dvRange), ...
+[iAF, ~, iMTT] = ind2sub([length(afRange), length(dvRange), ...
     length(mttRange)], index);
 af = afRange(iAF);
-dv = dvRange(iDV);
 mtt = mttRange(iMTT);
 
-% Scale the DV value
+% Calculate the DV value
 vecLenMatchCurve = sqrt(sum(dict(:, index).^2));
 vecLenCurve = sqrt(sum(curve.^2));
-dvScaleFactor = vecLenCurve / vecLenMatchCurve;
-dv = dv * dvScaleFactor;
+dv = vecLenCurve / vecLenMatchCurve;
 dv(dv > 1) = 1; % Clamp DV's value between 0 and 1 (known range)
 dv(dv < 0) = 0;
 
