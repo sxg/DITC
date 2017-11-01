9826695624225b58e5a4ac68d3dec9621322013f
Satyam Ghodasara <sghodas@gmail.com>
2017-11-01 15:18:39 -0400

    Used Katie's method to generate contrast from signal

--- signal2contrast.m
+++ signal2contrast.m
@@ -7,13 +7,18 @@ T10l = 800;
 R10l = 1/T10l;
 relaxivity = 6.3;
 
-liverStart = 10;
+% liverStart = 10;
+% 
+% S0l = mean(signal(2:2+liverStart)) ...
+%     * (1 - exp(-R10l * TR) * cos(alpha)) / (1 - exp(-R10l * TR)) ...
+%     / sin(alpha);
+% R1l = abs(log((S0l * sin(alpha) - signal .* cos(alpha)) ...
+%     ./ (S0l * sin(alpha) - signal)) / TR);
+% contrast = (R1l - R10l) * 1e3 / relaxivity;
 
-S0l = mean(signal(2:2+liverStart)) ...
-    * (1 - exp(-R10l * TR) * cos(alpha)) / (1 - exp(-R10l * TR)) ...
-    / sin(alpha);
-R1l = abs(log((S0l * sin(alpha) - signal .* cos(alpha)) ...
-    ./ (S0l * sin(alpha) - signal)) / TR);
-contrast = (R1l - R10l) * 1e3 / relaxivity;
+M0 = 0.0029;
+E1 = (signal - M0 * sin(alpha)) ./ (signal .* cos(alpha) - M0 .* sin(alpha));
+T1l = -TR./ log(E1); 
+contrast = (1/(relaxivity/1000)) .* ((1./T1l) - (1/T10l));
 
 end
