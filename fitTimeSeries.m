43e8524ac59e6dd616466cdc1985c7ee58f42351
Satyam Ghodasara <sghodas@gmail.com>
2017-11-04 14:55:26 -0400

    Updated fitTimeSeries to use parfor

--- fitTimeSeries.m
+++ fitTimeSeries.m
@@ -20,8 +20,8 @@ l = size(timeSeries, 1);
 w = size(timeSeries, 2);
 d = size(timeSeries, 3);
 t = size(timeSeries, 4);
-fitPerfParams = zeros(l, w, d, 7);
-fitCurves = zeros(l, w, d, t);
+fitPerfParamsList = zeros(l * w * d, 7);
+fitCurvesList = zeros(l * w * d, t);
 
 % Calculate the contrast concentrations
 artContrast = artSignal2contrast(artSignal, pvSignal, flipAngle, TR, ...
@@ -30,34 +30,31 @@ pvContrast = pvSignal2contrast(pvSignal, flipAngle, TR, T10p, ...
     relaxivity, startFrame, endFrame); 
 
 % Get the linear indexes from the mask
-indexList = find(mask);
-[i, j, k] = ind2sub([l, w, d], indexList);
+voxelIndexes = find(mask);
+[i, j, k] = ind2sub([l, w, d], voxelIndexes);
+voxelList(voxelIndexes, :) = timeSeries(i, j, k, :);
+nVoxels = size(voxelList, 1);
 
 %% Fit the perfusion parameters
 
-dispstat('', 'init');
+parfor_progress(nVoxels);
 t = tic; % Start the timer
-for index = 1:length(indexList)
-    progress = index / length(indexList);
-    elapsedTime = toc(t) / 60;
-    estimatedTime = elapsedTime / progress;
-    dispstat(sprintf( ...
-        ['Progress: %.2f%%\n' ...
-        'Elapsed Time: %.2f min\n' ...
-        'Estimated Time: %.2f min'], ...
-        100 * progress, elapsedTime, estimatedTime));
-    voxel = squeeze(timeSeries(i(index), j(index), k(index), :));
+parfor index = 1:nVoxels
+    voxel = voxelList(index, :);
     contrast = signal2contrast(voxel, flipAngle, TR, T10l, relaxivity, ...
          scaleFactor, startFrame, endFrame);
-    [f, ps, v2, af, v1, t1, tauA, ~] = lsFitCurve(contrast, times, ...
+    [f, ps, v2, af, v1, t1, tauA, ~] = lsFitCurve(contrast', times, ...
         artContrast, pvContrast);
-    fitPerfParams(i(index), j(index), k(index), :) = ...
-        [f, ps, v2, af, v1, t1, tauA];
-    fitCurves(i(index), j(index), k(index), :) = ...
-        normc(ditc([f, ps, v2, af, v1, tauA], ...
+    fitPerfParamsList(index, :) = [f, ps, v2, af, v1, t1, tauA];
+    fitCurvesList(index, :) = normc(ditc([f, ps, v2, af, v1, tauA], ...
         [artContrast, pvContrast, times]));
+    parfor_progress;
 end
 time = toc(t); % Stop the timer
+parfor_progress(0);
+
+fitPerfParams(i, j, k, :) = fitPerfParamsList(voxelIndexes, :);
+fitCurves(i, j, k, :) = fitCurvesList(voxelIndexes, :);
 
 %% Save the data
 save(sprintf('fitPerfusionVolume-%s.mat', saveFileSuffix), ...
